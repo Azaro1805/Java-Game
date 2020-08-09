346c41b93e85354818d894cc53748e1babc72c82
Or Azar <67314843+Azaro1805@users.noreply.github.com>
2020-08-09 16:06:32 +0300

    Lanucher
    Lanucher of the game

---
+++ GameFrame/Launcher.java
@@ -0,0 +1,13 @@
+package dev.codenmore.GameFrame;
+
+import dev.codenmore.GameFrame.Display.Display;
+
+public class Launcher {
+
+	public static void main (String[] args) {
+		
+		new Display("Title!" , 300 , 300 );
+		
+	}// main
+	
+}//class launcher 
