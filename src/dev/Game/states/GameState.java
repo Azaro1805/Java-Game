ba7c839a7996ef2558c96ad2741f40123b6cbec1
Or Azar <67314843+Azaro1805@users.noreply.github.com>
2020-08-11 19:53:32 +0300

    add states

---
+++ src/dev/Game/states/GameState.java
@@ -0,0 +1,27 @@
+package dev.Game.states;
+
+import java.awt.Graphics;
+
+import dev.Game.Game;
+import dev.Game.Entities.Creatures.Player;
+
+public class GameState extends State {
+
+	private Player player;
+
+	public GameState(Game game) {
+		super(game);
+		player = new Player (game, 100, 100);
+	}
+	
+	@Override
+	public void tick() {
+		player.tick();
+	}
+	
+	@Override
+	public void render(Graphics g) {
+		player.render(g);
+	}
+
+}// State
---
+++ src/dev/Game/states/MenuState.java
@@ -0,0 +1,24 @@
+package dev.Game.states;
+
+import java.awt.Graphics;
+
+import dev.Game.Game;
+
+public class MenuState extends State {
+	
+	public  MenuState (Game game) {
+		super(game);
+	}
+	
+	@Override
+	public void tick() {
+		
+	}
+	
+	@Override
+	public void render(Graphics g) {
+		
+	}
+
+	
+}
