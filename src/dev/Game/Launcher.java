f64c750c6e476e9d5e4ff764b42156c61ea857a6
Or Azar <67314843+Azaro1805@users.noreply.github.com>
2020-08-11 19:54:15 +0300

    launcher update

---
+++ src/dev/Game/Launcher.java
@@ -0,0 +1,13 @@
+package dev.Game;
+
+//import dev.codenmore.GameFrame.Display.Display;
+
+public class Launcher {
+
+	public static void main (String[] args) {
+		
+		Game game = new Game("Or Game!" , 640 , 360 );
+		game.start();
+	}// main
+	
+}//class launcher 
