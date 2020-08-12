6716f065b51d85cb20a9842caa4d97e50e6d850e
Or Azar <67314843+Azaro1805@users.noreply.github.com>
2020-08-12 19:12:51 +0300

    Update Player.java

--- src/dev/Game/Entities/Creatures/Player.java
+++ src/dev/Game/Entities/Creatures/Player.java
@@ -10,33 +10,33 @@ public class Player extends Creature {
 	private Game game;
 
 	public Player( Game game ,float x, float y) {
-		super(x, y);
+		super(x, y , Creature.DEFAULT_WIDTH, Creature.DEFAULT_HEIGHT*2 );
 		this.game = game;
 	}
-
+	
 	@Override
 	public void tick() {
-		if (game.getKeyManager().up)
-			y -=3;
-		if (game.getKeyManager().upw)
-			y -=3;
-		if (game.getKeyManager().down)
-			y +=3;
-		if (game.getKeyManager().downs)
-			y +=3;
-		if (game.getKeyManager().right)
-			x +=3;
-		if (game.getKeyManager().rightd)
-			x +=3;
-		if (game.getKeyManager().left)
-			x -=3;
-		if (game.getKeyManager().lefta)
-			x -=3;
+		getInput();
+		move();
 	}
 
+	private void getInput() {
+		xMove = 0 ;
+		yMove = 0 ;
+		
+		if (game.getKeyManager().up || game.getKeyManager().upw )
+			yMove = -speed;
+		if (game.getKeyManager().down || game.getKeyManager().downs)
+			yMove = speed;
+		if (game.getKeyManager().right || game.getKeyManager().rightd)
+			xMove = speed;
+		if (game.getKeyManager().left || game.getKeyManager().lefta)
+			xMove = -speed;
+	}
+	
 	@Override
 	public void render(Graphics g) {
-		g.drawImage(Assets.player, (int) x, (int) y, null);
+		g.drawImage(Assets.player, (int) x, (int) y, width, height, null);
 	}
 
 
