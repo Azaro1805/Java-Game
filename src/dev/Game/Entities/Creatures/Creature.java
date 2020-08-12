ee14844322f34fbf8a789ce787e9405446a073ad
Or Azar <67314843+Azaro1805@users.noreply.github.com>
2020-08-12 19:12:46 +0300

    Update Creature.java

--- src/dev/Game/Entities/Creatures/Creature.java
+++ src/dev/Game/Entities/Creatures/Creature.java
@@ -4,11 +4,58 @@ import dev.Game.Entities.Entity;
 
 public abstract class Creature extends Entity {
 
+	public static final int DEFAULT_HEALTH = 10;
+	public static final float DEFAULT_SPEED = 3.0f;
+	public static final int DEFAULT_WIDTH = 35,
+							DEFAULT_HEIGHT=35;
 	protected int health;
+	protected float speed;
+	protected float xMove, yMove;
 	
-	public Creature(float x, float y) {
-		super(x, y);
-		health=10;
+	public Creature(float x, float y, int width, int height) {
+		super(x, y, width, height);
+		health = DEFAULT_HEALTH;
+		speed = DEFAULT_SPEED;
+		xMove = 0;
+		yMove = 0;
 	}
 
-}
+	public void move() {
+		x += xMove;
+		y += yMove;
+	}
+	
+	// Getter and Setters
+	public float getxMove() {
+		return xMove;
+	}
+
+	public void setxMove(float xMove) {
+		this.xMove = xMove;
+	}
+
+	public float getyMove() {
+		return yMove;
+	}
+
+	public void setyMove(float yMove) {
+		this.yMove = yMove;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public float getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(float speed) {
+		this.speed = speed;
+	}
+
+} //Creature
