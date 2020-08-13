28e5c86d320361558e6d9fae85dc57325bd7cfc8
Or Azar <67314843+Azaro1805@users.noreply.github.com>
2020-08-13 15:35:10 +0300

    Update Assets.java

--- src/dev/Game/gfx/Assets.java
+++ src/dev/Game/gfx/Assets.java
@@ -7,7 +7,7 @@ public class Assets {
 	// the size of every one tile
 	private static final int width=35 , height =35; 
 	
-	public static BufferedImage player, dirt, grass, stone, tree , stump;	
+	public static BufferedImage player, dirt, grass, stone, tree , stump , rock;	
 	
 	public static void init () {
 		SpriteSheet sheet = new SpriteSheet (ImageLoader.loadImage("/textures/objects22.png"));
@@ -15,7 +15,9 @@ public class Assets {
 		stump = sheet.crop(0, 0, width, height);
 		dirt = sheet.crop(width, 0, width, height);
 		grass = sheet.crop(2*width, 0, width, height);
+		rock = sheet.crop(3*width, 0, width, height);
 		player = sheet.crop(0, height+1, width, 2*height);
+
 	}
 	
 }// Assets
