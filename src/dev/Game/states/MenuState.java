b8f30738eea364e4f8b0f5beba5cd47a25841c6a
Or Azar <67314843+Azaro1805@users.noreply.github.com>
2020-08-14 17:57:07 +0300

    Update MenuState.java

--- src/dev/Game/states/MenuState.java
+++ src/dev/Game/states/MenuState.java
@@ -3,11 +3,12 @@ package dev.Game.states;
 import java.awt.Graphics;
 
 import dev.Game.Game;
+import dev.Game.Handler;
 
 public class MenuState extends State {
 	
-	public  MenuState (Game game) {
-		super(game);
+	public  MenuState (Handler handler) {
+		super(handler);
 	}
 	
 	@Override
