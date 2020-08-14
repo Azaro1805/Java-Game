8b72796899073ac53ebb2ca8f51eed9c22ddcab1
Or Azar <67314843+Azaro1805@users.noreply.github.com>
2020-08-14 17:56:54 +0300

    Update Entity.java

--- src/dev/Game/Entities/Entity.java
+++ src/dev/Game/Entities/Entity.java
@@ -2,13 +2,17 @@ package dev.Game.Entities;
 
 import java.awt.Graphics;
 
+import dev.Game.Handler;
+
 public abstract class Entity {
 
 	//all extends class have access
+	protected Handler handler;
 	protected float x, y;
 	protected int width, height;
 
-	public Entity(float x, float y,  int width, int height ) {
+	public Entity(Handler handler, float x, float y,  int width, int height ) {
+		this.handler = handler;
 		this.x = x;
 		this.y = y;
 		this.height=height;
