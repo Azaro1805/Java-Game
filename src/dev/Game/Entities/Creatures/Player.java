ee7b59a2080a6a43a8e79619497e9c7fb3817f58
Or Azar <67314843+Azaro1805@users.noreply.github.com>
2020-08-14 17:56:51 +0300

    Update Player.java

--- src/dev/Game/Entities/Creatures/Player.java
+++ src/dev/Game/Entities/Creatures/Player.java
@@ -1,42 +1,40 @@
 package dev.Game.Entities.Creatures;
 
 import java.awt.Graphics;
-
-import dev.Game.Game;
+import dev.Game.Handler;
 import dev.Game.gfx.Assets;
 
 public class Player extends Creature {
 
-	private Game game;
 
-	public Player( Game game ,float x, float y) {
-		super(x, y , Creature.DEFAULT_WIDTH, Creature.DEFAULT_HEIGHT*2 );
-		this.game = game;
+	public Player( Handler handler ,float x, float y) {
+		super(handler, x, y, Creature.DEFAULT_WIDTH, Creature.DEFAULT_HEIGHT*2 );
 	}
 	
 	@Override
 	public void tick() {
 		getInput();
 		move();
+		handler.getGameCamera().ceterCamrea(this);
 	}
 
 	private void getInput() {
 		xMove = 0 ;
 		yMove = 0 ;
 		
-		if (game.getKeyManager().up || game.getKeyManager().upw )
+		if (handler.getKeyManager().up || handler.getKeyManager().upw )
 			yMove = -speed;
-		if (game.getKeyManager().down || game.getKeyManager().downs)
+		if (handler.getKeyManager().down || handler.getKeyManager().downs)
 			yMove = speed;
-		if (game.getKeyManager().right || game.getKeyManager().rightd)
+		if (handler.getKeyManager().right || handler.getKeyManager().rightd)
 			xMove = speed;
-		if (game.getKeyManager().left || game.getKeyManager().lefta)
+		if (handler.getKeyManager().left || handler.getKeyManager().lefta)
 			xMove = -speed;
 	}
 	
 	@Override
 	public void render(Graphics g) {
-		g.drawImage(Assets.player, (int) x, (int) y, width, height, null);
+		g.drawImage(Assets.player, (int) (x - handler.getGameCamera().getxOffset()) , (int) (y - handler.getGameCamera().getyOffset()), width, height, null);
 	}
 
 
