859ddab80b009fdf2980dedd83ab2af6953aeda9
Or Azar <67314843+Azaro1805@users.noreply.github.com>
2020-08-16 00:42:33 +0300

    Update Player.java

--- src/dev/Game/Entities/Creatures/Player.java
+++ src/dev/Game/Entities/Creatures/Player.java
@@ -11,10 +11,10 @@ public class Player extends Creature {
 	public Player( Handler handler ,float x, float y) {
 		super(handler, x, y, Creature.DEFAULT_WIDTH, Creature.DEFAULT_HEIGHT*2 );
 		
-		bounds.x = 0; //17
-		bounds.y = 35;
-		bounds.width = 35;
-		bounds.height =35;
+		//bounds.x = 0; //17
+		//bounds.y = 35;
+		//bounds.width = 35;
+		//bounds.height =35;
 	}
 	
 	@Override
@@ -42,10 +42,10 @@ public class Player extends Creature {
 	public void render(Graphics g) {
 		g.drawImage(Assets.player, (int) (x - handler.getGameCamera().getxOffset()) , (int) (y - handler.getGameCamera().getyOffset()), width, height, null);
 		
-		//g.setColor(Color.red);
-		//g.fillRect((int) (x + bounds.x - handler.getGameCamera().getxOffset()),
+	//	g.setColor(Color.red);
+	//	g.fillRect((int) (x + bounds.x - handler.getGameCamera().getxOffset()),
 		//		(int) (y + bounds.y - handler.getGameCamera().getyOffset()),
-		//		bounds.width, bounds.height);
+			//	bounds.width, bounds.height);
 	}
 
 
