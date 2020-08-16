786fe79e374d5157db1a83898bcf1f8cb2c13ecb
Or Azar <67314843+Azaro1805@users.noreply.github.com>
2020-08-17 01:01:28 +0300

    Update GameState.java

--- src/dev/Game/states/GameState.java
+++ src/dev/Game/states/GameState.java
@@ -2,34 +2,30 @@ package dev.Game.states;
 
 import java.awt.Graphics;
 
-import dev.Game.Game;
 import dev.Game.Handler;
 import dev.Game.Entities.Creatures.Player;
+import dev.Game.Entities.Statics.Tree;
 import dev.Game.worlds.World;
 
 public class GameState extends State {
 
-	private Player player;
 	private World world;
 
 	public GameState(Handler handler) {
 		super(handler);
 		world = new World(handler, "res/World/world1.txt");
 		handler.setWorld(world);
-		player = new Player (handler, 100, 100);
 		
 	}
 	
 	@Override
 	public void tick() {
 		world.tick();
-		player.tick();
 	}
 	
 	@Override
 	public void render(Graphics g) {
 		world.render(g);
-		player.render(g);
 
 	}
 
