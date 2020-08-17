84375648a94e355b1e77483e5235a414d4603812
Or Azar <67314843+Azaro1805@users.noreply.github.com>
2020-08-17 17:59:37 +0300

    Update EntityManager.java

--- src/dev/Game/Entities/EntityManager.java
+++ src/dev/Game/Entities/EntityManager.java
@@ -2,37 +2,46 @@ package dev.Game.Entities;
 
 import java.awt.Graphics;
 import java.util.ArrayList;
-
+import java.util.Comparator;
 import dev.Game.Handler;
 import dev.Game.Entities.Creatures.Player;
 
 public class EntityManager {
-	
+
 	private Handler handler;
 	private Player player;
 	private ArrayList <Entity> entities;
+	private Comparator <Entity> renderSorter =  new Comparator <Entity>() {
+
+		@Override
+		public int compare(Entity a, Entity b) {
+			if (a.getY() + a.getHeight() < b.getY() + b.getHeight())
+				return -1;
+			return 0;
+		}
+	};
 
 	public EntityManager(Handler handler, Player player) {
 		this.handler = handler;
 		this.player = player;
 		entities =  new ArrayList<Entity>();
+		addEntity(player);
 	}
-	
+
 	public void tick () {
 		for (int i = 0; i <  entities.size(); i++) {
 			Entity e = entities.get(i);
 			e.tick();
 		}
-		player.tick();
+		entities.sort(renderSorter);
 	}
-	
+
 	public void render(Graphics g) {
 		for (Entity e : entities) {
 			e.render(g);
 		}
-		player.render(g);
 	}
-	
+
 	public void addEntity( Entity e) {
 		entities.add(e);
 	}
@@ -60,8 +69,8 @@ public class EntityManager {
 	public void setEntities(ArrayList<Entity> entities) {
 		this.entities = entities;
 	}
-	
+
 	//Getters & Setters
-	
-	
+
+
 }//EntityManager
