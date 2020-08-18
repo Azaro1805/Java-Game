c3fbeff540483a90e55b7a90a1bb837dc4b97b74
Or Azar <67314843+Azaro1805@users.noreply.github.com>
2020-08-18 19:26:35 +0300

    Update MenuState.java

--- src/dev/Game/states/MenuState.java
+++ src/dev/Game/states/MenuState.java
@@ -2,24 +2,40 @@ package dev.Game.states;
 
 import java.awt.Graphics;
 
-import dev.Game.Game;
 import dev.Game.Handler;
+import dev.Game.gfx.Assets;
+import dev.Game.ui.ClickListener;
+import dev.Game.ui.UIImageButton;
+import dev.Game.ui.UIManager;
 
 public class MenuState extends State {
-	
+
+	private UIManager uiManager; 
+
 	public  MenuState (Handler handler) {
 		super(handler);
-	}
-	
-	@Override
-	public void tick() {
-		//System.out.println(handler.getMouseManager().getMouseX());
-	}
-	
-	@Override
-	public void render(Graphics g) {
-		
+		uiManager =  new UIManager(handler);
+
+		uiManager.addObjects(new UIImageButton(200,200, 128, 64, Assets.btn_start, new ClickListener() {
+
+			@Override
+			// this is what happened if we click on the new button that we made right-now
+			public void onClick() {
+				State.setState(handler.getGame().gameState);
+			}
+		} ));
+
 	}
 
-	
+	@Override
+	public void tick() {
+		uiManager.tick();
+	}
+
+	@Override
+	public void render(Graphics g) {
+		uiManager.render(g);
+	}
+
+
 }
