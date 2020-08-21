edfcd4a2822898dfa968e773357f2daa9dc45d41
Or Azar <67314843+Azaro1805@users.noreply.github.com>
2020-08-21 17:44:31 +0300

    Create Item.java

---
+++ src/dev/Game/items/Item.java
@@ -0,0 +1,114 @@
+package dev.Game.items;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import dev.Game.Handler;
+import dev.Game.gfx.Assets;
+
+public class Item {
+	
+	//Handler
+	public static Item[] items =  new Item[256];
+	public static Item WoodItem = new Item(Assets.timber, "Wood", 0); // create item 
+	public static Item RockItem = new Item(Assets.rock, "Rock", 1);
+	
+	//Class
+
+	public static final int ITEMWIDTH = 34, ITEMHEIGHT = 34, PICKED_UP = -1;
+
+	protected Handler handler;
+	protected BufferedImage texture;
+	protected String name;
+	protected final int id;
+	protected int x, y, count;
+
+
+	public Item (BufferedImage texture, String name, int id ) {
+		this.texture = texture;
+		this.id = id;
+		this.name = name;
+		count = 1;
+		
+		items[id] = this;
+	}
+
+	public void tick() {
+
+	}
+
+	public void render (Graphics g) {
+		if(handler == null)
+			return;
+		render(g,(int) (x - handler.getGameCamera().getxOffset()), (int) (y - handler.getGameCamera().getyOffset()));
+	}
+
+	public void render (Graphics g, int x, int y) {
+		g.drawImage(texture, x, y, ITEMWIDTH, ITEMHEIGHT, null);
+	}
+
+	public Item createNew(int x, int y) {
+		Item i = new Item(texture, name, id);
+		i.setPosition(x, y);
+		return i;
+	}
+	
+	public void setPosition(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	//Getters & Setters
+	
+	public Handler getHandler() {
+		return handler;
+	}
+
+	public void setHandler(Handler handler) {
+		this.handler = handler;
+	}
+
+	public BufferedImage getTexture() {
+		return texture;
+	}
+
+	public void setTexture(BufferedImage texture) {
+		this.texture = texture;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+}//Item
