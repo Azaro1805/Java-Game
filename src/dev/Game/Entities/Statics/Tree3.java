933efceb626fe34d10062a1bde69b36415671512
Or Azar <67314843+Azaro1805@users.noreply.github.com>
2020-08-21 17:44:26 +0300

    Create Tree3.java

---
+++ src/dev/Game/Entities/Statics/Tree3.java
@@ -0,0 +1,37 @@
+package dev.Game.Entities.Statics;
+
+import java.awt.Graphics;
+
+import dev.Game.Handler;
+import dev.Game.gfx.Assets;
+import dev.Game.items.Item;
+import dev.Game.tiles.Tile;
+
+public class Tree3 extends StaticEntity{
+
+	public Tree3(Handler handler, float x, float y) {
+		super(handler, x, y, Tile.TILEWIDTH, Tile.TILEHEIGHT*2); 
+		//Tile.TILEWIDTH, Tile.TILEHEIGHT - is the size of the tree in draw
+		
+		bounds.x = 10;
+		bounds.y = (int) (height /1.5f);
+		bounds.height = (int) (height - height /1.5f);
+		bounds.width = width - 20;
+	}
+
+	@Override
+	public void tick() {
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		g.drawImage(Assets.tree3, (int) (x - handler.getGameCamera().getxOffset()), (int) (y - handler.getGameCamera().getyOffset()), width, height, null);
+	}
+
+	@Override
+	public void die() {
+		handler.getWorld().getItemManager().addItem(Item.WoodItem.createNew((int) x, (int) y));
+	}
+	
+}
