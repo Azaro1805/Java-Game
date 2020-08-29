19d0421768d9e0782b6b1ada9df21747fe9cd377
Or Azar <67314843+Azaro1805@users.noreply.github.com>
2020-08-29 23:22:30 +0300

    Update Tree.java

--- src/dev/Game/Entities/Statics/Tree.java
+++ src/dev/Game/Entities/Statics/Tree.java
@@ -31,7 +31,7 @@ public class Tree extends StaticEntity{
 
 	@Override
 	public void die() {
-		handler.getWorld().getItemManager().addItem(Item.WoodItem.createNew((int) x, (int) y));
+		handler.getWorld().getItemManager().addItem(Item.WoodItem.createNew((int) x, (int) y-40));
 	}
 	
 }
