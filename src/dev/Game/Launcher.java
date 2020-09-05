f32ec4762eda90f4d6ee8d16ca67326647de8788
Or Azar <67314843+Azaro1805@users.noreply.github.com>
2020-09-05 19:54:02 +0300

    Update Launcher.java

--- src/dev/Game/Launcher.java
+++ src/dev/Game/Launcher.java
@@ -6,7 +6,7 @@ public class Launcher {
 
 	public static void main (String[] args) {
 		
-		Game game = new Game("Or Game!" , 800 , 500 );
+		Game game = new Game("Or Game!" , 900 , 600 );
 		game.start();
 	}// main
 	
