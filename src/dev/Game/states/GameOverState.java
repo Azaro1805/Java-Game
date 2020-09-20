c0a7f62b151a9285d95f2c443f4a46d6d2467f5e
Or Azar <67314843+Azaro1805@users.noreply.github.com>
2020-09-20 20:16:51 +0300

    Create GameOverState.java

---
+++ src/dev/Game/states/GameOverState.java
@@ -0,0 +1,43 @@
+package dev.Game.states;
+
+import java.awt.Graphics;
+
+import dev.Game.Handler;
+import dev.Game.gfx.Assets;
+import dev.Game.ui.ClickListener;
+import dev.Game.ui.UIImageButton;
+import dev.Game.ui.UIManager;
+
+public class GameOverState extends State {
+
+	private UIManager uiManager; 
+
+	public  GameOverState (Handler handler) {
+		super(handler);
+		uiManager =  new UIManager(handler);
+		handler.getMouseManager().setUIManager(uiManager);
+		
+		uiManager.addObjects(new UIImageButton(350,200, 128, 64, Assets.btn_start, new ClickListener() {
+
+			@Override
+			// this is what happened if we click on the new button that we made right-now
+			public void onClick() {
+				handler.getMouseManager().setUIManager(null);
+				State.setState(handler.getGame().gameState);
+			}
+		} ));
+
+	}
+
+	@Override
+	public void tick() {
+		uiManager.tick();
+	}
+
+	@Override
+	public void render(Graphics g) {
+		uiManager.render(g);
+	}
+
+
+}
