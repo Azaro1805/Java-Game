becaf2eca1164a26b9e36465f55efa8a711de6b8
Or Azar <67314843+Azaro1805@users.noreply.github.com>
2020-09-23 16:14:26 +0300

    Update StaticEntity.java

--- src/dev/Game/Entities/Statics/StaticEntity.java
+++ src/dev/Game/Entities/Statics/StaticEntity.java
@@ -11,5 +11,8 @@ public abstract class StaticEntity extends Entity{
 		super(handler, x, y, width, height);
 	}
 
+	public StaticEntity(Handler handler, float x, float y, int width, int height , int health) {
+		super(handler, x, y, width, height, health);
+	}
 
 }//StaticEntity
